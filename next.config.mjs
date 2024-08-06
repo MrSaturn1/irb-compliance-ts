be43257120844eac0909a27f730e25cb9513c86d
Ian Sears <iansears11@gmail.com>
2024-08-06 16:33:06 -0700

    vercel.json changes

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
 const nextConfig = {
+  distDir: '.next',
   typescript: {
     tsconfigPath: './tsconfig.json',
     ignoreBuildErrors: false,
