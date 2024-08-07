070aee426c42ee15b8d835220e787accf8090766
Ian Sears <iansears11@gmail.com>
2024-08-07 16:09:34 -0700

    convert port to a number

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -120,7 +120,7 @@ app.post('/api/add-document', upload.single('file'), async (req, res) => {
 });
 
 // Start Server
-app.listen(port, '0.0.0.0', () => {
+app.listen(Number(port), '0.0.0.0', () => {
   console.log(`Server running on http://0.0.0.0:${port}`);
   initializeRAGSystem();
 });
